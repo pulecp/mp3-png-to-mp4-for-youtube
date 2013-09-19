50f7ce6d273f6c4e8d001b887c6eeccb5730c4d2
Pavel Pulec <pavel.pulec@virtualmaster.com>
2013-09-19 14:46:13 +0200

    init commit

---
+++ mp3-png-to-mp4-for-youtube.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# $1 = image.png
+# $2 = sound.mp3
+# $3 = length of mp3
+
+if [ $# -ne 3 ];then echo "Usage: ./$0 image.png sound.mp3 length_of_mp3_as_number"; echo "Exiting...";exit 1;fi
+
+ffmpeg -loop 1 -i $1 -t $3 silent$$.mp4
+ffmpeg -i silent$$.mp4 -i $2 -vcodec copy -acodec copy out.mp4
+
+rm silent$$.mp4
+echo "Done, out.mp4 created"
