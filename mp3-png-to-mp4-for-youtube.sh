a3129ceb7c1c85a761aedbc5bc3458169df35bd5
Pavel Pulec <pavel.pulec@virtualmaster.com>
2013-09-19 14:50:05 +0200

    thanks ubuntuforums.org

--- mp3-png-to-mp4-for-youtube.sh
+++ mp3-png-to-mp4-for-youtube.sh
@@ -4,6 +4,8 @@
 # $2 = sound.mp3
 # $3 = length of mp3
 
+# thanks ron999 in this tread http://ubuntuforums.org/showthread.php?t=1502537
+
 if [ $# -ne 3 ];then echo "Usage: ./$0 image.png sound.mp3 length_of_mp3_as_number"; echo "Exiting...";exit 1;fi
 
 ffmpeg -loop 1 -i $1 -t $3 silent$$.mp4
