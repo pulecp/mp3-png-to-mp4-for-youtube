13c9720543e12203058e12f059d7baa1913bde82
George Zinyakov <gz.dev@icloud.com>
2017-06-28 14:11:29 +0300

    mp3 to mp4 directly with 720p

--- mp3-png-to-mp4-for-youtube.sh
+++ mp3-png-to-mp4-for-youtube.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-# $1 = "image with any name.png"
-# $2 = "sound with any name.mp3"
-# $3 = length of mp3
+# $1 = "sound with any name.mp3"
 
 # thanks ron999 in this tread http://ubuntuforums.org/showthread.php?t=1502537
 
-if [ $# -ne 2 ]; then 
-	echo 'Usage: ./$0 "image with any name.png" "sound with any name.mp3"';
+if [ $# -ne 1 ]; then 
+	echo 'Usage: ./$0 "sound with any name.mp3"';
 	echo "Exiting...";
 	exit 1;
 fi
 
-length=`sox "$2" -n stat 2>&1 | sed -n 's#^Length (seconds):[^0-9]*\([0-9.]*\)$#\1#p'`
-h=`basename "$2" .mp3`.mp4
+length=`sox "$1" -n stat 2>&1 | sed -n 's#^Length (seconds):[^0-9]*\([0-9.]*\)$#\1#p'`
+h=`basename "$1" .mp3`.mp4
 
-ffmpeg -loop 1 -i "$1" -t $length silent$$.mp4
-ffmpeg -i silent$$.mp4 -i "$2" -vcodec copy -acodec copy "$h"
+ffmpeg -i "$1" -vf "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:(ow-iw)/2:(oh-ih)/2" cover$$.png
+ffmpeg -loop 1 -i cover$$.png -t $length silent$$.mp4
+ffmpeg -i silent$$.mp4 -i "$1" -vcodec copy -acodec copy "$h"
 
 rm silent$$.mp4
+rm cover$$.png
 echo "Done, $h created"
