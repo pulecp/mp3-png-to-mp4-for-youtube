3a16c38cafd51ec04f55891c1c19c10bc40ce855
George Zinyakov <gz.dev@icloud.com>
2017-06-28 04:15:41 +0300

    Using any names for sound and image files

--- mp3-png-to-mp4-for-youtube.sh
+++ mp3-png-to-mp4-for-youtube.sh
@@ -6,13 +6,17 @@
 
 # thanks ron999 in this tread http://ubuntuforums.org/showthread.php?t=1502537
 
-if [ $# -ne 2 ];then echo "Usage: ./$0 image.png sound.mp3"; echo "Exiting...";exit 1;fi
+if [ $# -ne 2 ]; then 
+	echo "Usage: ./$0 image.png sound.mp3";
+	echo "Exiting...";
+	exit 1;
+fi
 
-length=`sox $2 -n stat 2>&1 | sed -n 's#^Length (seconds):[^0-9]*\([0-9.]*\)$#\1#p'`
-h=`basename $2 .mp3`.mp4
+length=`sox "$2" -n stat 2>&1 | sed -n 's#^Length (seconds):[^0-9]*\([0-9.]*\)$#\1#p'`
+h=`basename "$2" .mp3`.mp4
 
-ffmpeg -loop 1 -i $1 -t $length silent$$.mp4
-ffmpeg -i silent$$.mp4 -i $2 -vcodec copy -acodec copy $h
+ffmpeg -loop 1 -i "$1" -t $length silent$$.mp4
+ffmpeg -i silent$$.mp4 -i "$2" -vcodec copy -acodec copy "$h"
 
 rm silent$$.mp4
-echo "Done, out.mp4 created"
+echo "Done, $h created"
