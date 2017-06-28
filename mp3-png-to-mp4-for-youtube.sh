1debb5af94e3fd8e65766ab3c7ed0f0e962d1396
George Zinyakov <gz.dev@icloud.com>
2017-06-28 21:38:07 +0300

    Set video size to aspect ratio of original image of mp3 file with 720p scale

--- mp3-png-to-mp4-for-youtube.sh
+++ mp3-png-to-mp4-for-youtube.sh
@@ -13,7 +13,7 @@ fi
 length=`sox "$1" -n stat 2>&1 | sed -n 's#^Length (seconds):[^0-9]*\([0-9.]*\)$#\1#p'`
 h=`basename "$1" .mp3`.mp4
 
-ffmpeg -i "$1" -vf "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:(ow-iw)/2:(oh-ih)/2" cover$$.png
+ffmpeg -i "$1"  -filter:v scale=-1:720 -c:a copy cover$$.png
 ffmpeg -loop 1 -i cover$$.png -t $length silent$$.mp4
 ffmpeg -i silent$$.mp4 -i "$1" -vcodec copy -acodec copy "$h"
 
